b0dfcd6281a0dd580ecae5a6fcc911ba13d5adf0
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-12-29 01:00:42 +0200

    feat(config): allow non free packages

--- configuration.nix
+++ configuration.nix
@@ -28,11 +28,12 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-#  console = {
-#	  font = "Lat2-Terminus16";
-#   keyMap = "us";
-#   useXkbConfig = true; # use xkb.options in tty.
-  #};
+
+  nixpkgs = {
+	  config = {
+		  allowUnfree = true;
+	  };
+  };
 
   hardware.graphics.enable = true;
 programs.hyprland.enable = true;
