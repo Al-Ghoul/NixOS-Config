fe04db9ba208ec05ae070ceeab99612ad46aaef5
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-12-28 08:16:33 +0000

    feat(hyprland): add initial styles & colors

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -29,5 +29,31 @@
 			"$mod, mouse:273, resizewindow"
 		];
 
+		general = {
+			gaps_in = 5;
+			gaps_out= 5;
+			border_size = 2;
+			"col.active_border" = "rgba(13ACACee) rgba(16A0F3ee) 90deg";
+			"col.inactive_border" = "rgba(595959aa)";
+			layout = "dwindle";
+		};
+		misc = {
+			disable_hyprland_logo= true;		
+		};
+		dwindle = {
+			pseudotile = true;
+			preserve_split = true;
+		};
+		decoration = {
+			rounding = 5;
+			blur = {
+				enabled = true;
+				size = 5;
+				passes = 3;
+				ignore_opacity = true;
+				new_optimizations = true;
+				popups = true;
+			};
+		};
 	};
 }
