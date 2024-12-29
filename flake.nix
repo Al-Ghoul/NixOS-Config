2309a0a1f4ddad966423b8bb7b1f8f00e0dd485f
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-12-29 02:45:16 +0200

    feat: add initial devshells

--- flake.nix
+++ flake.nix
@@ -1,38 +1,51 @@
 {
   description = "Abdo's NixOS configuration";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		home-manager.url = "github:nix-community/home-manager";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-		nixvim = {
-			url = "github:nix-community/nixvim";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
-
-
-  outputs = {  nixpkgs,home-manager, nixvim, ... }: {
+  outputs = {
+    nixpkgs,
+    home-manager,
+    nixvim,
+    devshell,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [devshell.overlays.default];
+    };
+  in {
+    devShells."${system}".default = pkgs.devshell.mkShell {imports = [(pkgs.devshell.importTOML ./devshell.toml)];};
 
     nixosConfigurations.AlGhoul = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
       modules = [
-	      ./configuration.nix
-		home-manager.nixosModules.home-manager
-		{
-			home-manager.useGlobalPkgs = true;
-			home-manager.useUserPackages = true;
-			home-manager.users.abdo = {
-				imports = [
-					./home-manager.nix
-					nixvim.homeManagerModules.nixvim
-				];
-			};
-		}
-      ] ;
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.abdo = {
+            imports = [
+              ./home-manager.nix
+              nixvim.homeManagerModules.nixvim
+            ];
+          };
+        }
+      ];
     };
-
-
   };
 }
