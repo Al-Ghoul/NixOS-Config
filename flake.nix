31cc6aea597faed0953b81e424286ba3ab866ff2
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-04-13 06:35:02 +0200

    feat: remove devshells

--- flake.nix
+++ flake.nix
@@ -8,10 +8,6 @@
     nixvim = {
       url = "github:nix-community/nixvim";
     };
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,18 +18,11 @@
     nixpkgs,
     home-manager,
     nixvim,
-    devshell,
     sops-nix,
     ...
   }: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [devshell.overlays.default];
-    };
   in {
-    devShells."${system}".default = pkgs.devshell.mkShell {imports = [(pkgs.devshell.importTOML ./devshell.toml)];};
-
     nixosConfigurations.AlGhoul = nixpkgs.lib.nixosSystem {
       inherit system;
       modules = [
@@ -42,13 +31,15 @@
 
         home-manager.nixosModules.home-manager
         {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.abdo = {
-            imports = [
-              ./home-manager.nix
-              nixvim.homeManagerModules.nixvim
-            ];
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.abdo = {
+              imports = [
+                ./home-manager.nix
+                nixvim.homeManagerModules.nixvim
+              ];
+            };
           };
         }
       ];
