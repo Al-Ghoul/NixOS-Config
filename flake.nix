1e1ea80810c9e01f209ae6f588d0a02d1997f26b
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-06-28 14:12:17 +0300

    feat: add treefmt

--- flake.nix
+++ flake.nix
@@ -3,25 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nixvim = {
       url = "github:nix-community/nixvim";
     };
+
     sops-nix = {
       url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = {
+    self,
     nixpkgs,
     home-manager,
     nixvim,
     sops-nix,
+    treefmt-nix,
+    systems,
     ...
   }: let
     system = "x86_64-linux";
+    eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+
+    treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
   in {
     nixosConfigurations.AlGhoul = nixpkgs.lib.nixosSystem {
       inherit system;
@@ -44,5 +59,10 @@
         }
       ];
     };
+
+    formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+    checks = eachSystem (pkgs: {
+      formatting = treefmtEval.${pkgs.system}.config.build.check self;
+    });
   };
 }
